de8ec572be90b9e4d410911dc3a2607c7ca768b3
Andres Pozo <andpozo@gmail.com>
2020-02-24 01:28:24 -0300

    fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ttext',
-    version='0.0.1',
+    version='0.0.2',
     description='A library for auto-converting URLs, mentions, hashtags, lists, etc. in Twitter text. Also does tweet validation and search term highlighting.',
     author='Gain App',
     author_email='developers@gainapp.com',
